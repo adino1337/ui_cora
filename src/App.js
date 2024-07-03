745f0718c99cf68733b63a7b51cbf71705b40dc1
xtejbus <xtejbus@fi.muni,cz>
2024-07-03 10:41:46 +0200

    made new app component for main UIGenerator

--- src/App.js
+++ src/App.js
@@ -1,111 +1,53 @@
-import { useState, useEffect } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-import "./App.css";
-import schema from "./schemas/schema_3.json";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Marks from "./components/Marks/Marks";
-import UiBlockPanel from "./components/UiBlockPanel/UiBlockPanel";
-import TitleForm from "./components/TitlePanel/TitleForm/TitleForm";
-import TitlePanel from "./components/TitlePanel/TitlePanel";
-import BuildArea from "./components/BuildArea/BuildArea";
-import EditButtons from "./components/EditButtons/EditButtons";
-import { onDragEnd } from "./utils/onDragEnd";
-import buildAreaToSchemaMapper from "./utils/buildAreaToSchemaMapper";
+import React, { useEffect, useState } from "react";
+import UIGenerator from "./UIGenerator/UIGenerator";
+
 function App() {
-  const [uiBlocks, setUiBlocks] = useState(schema);
-  const [titleBlocks, setTitleBlocks] = useState([]);
-  const [buildArea, setBuildArea] = useState([]);
-  const [edit, setEdit] = useState(true);
-  const [dragEnd, setDragEnd] = useState(false);
+  const sleepTime = 300;
+  // fake async call
+  // UIschema is null until its not update call for a form
+  const [data, setData] = useState({
+    schema: null,
+    UISchema: null,
+    loading: true,
+  });
 
   useEffect(() => {
-    setBuildArea((prev) => {
-      return prev
-        .map((row) => row.filter((col) => col.length !== 0)) // Filter out empty columns
-        .filter((row) => row.length !== 0); // Filter out empty rows
-    });
-  }, [dragEnd]);
+    const fetchSchema = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, sleepTime));
+        const schema = await import("./schemas/schema_3.json");
+        const UIschema = await import("./schemas/UI_3.json");
+        setData({
+          schema: schema.default,
+          UISchema: UIschema.default,
+          loading: false,
+        });
+      } catch (error) {
+        setData({ schema: null, UISchema: null, loading: false });
+      }
+    };
 
-  const handleOnDragEnd = (result) => {
-    onDragEnd(
-      result,
-      buildArea,
-      setBuildArea,
-      setDragEnd,
-      uiBlocks,
-      titleBlocks
+    fetchSchema();
+  }, []);
+
+  if (data.loading) {
+    // Loading component will be prolly in retec code base
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          fontSize: "3rem",
+        }}
+      >
+        Loading...
+      </div>
     );
-  };
+  }
 
-  const deleteField = (rowIndex, columnIndex, fieldIndex, fieldType) => {
-    const movedField = buildArea[rowIndex][columnIndex][fieldIndex];
-    setBuildArea((prev) => {
-      prev[rowIndex][columnIndex] = prev[rowIndex][columnIndex].filter(
-        (_, fieldID) => fieldID !== fieldIndex
-      );
-      prev[rowIndex] = prev[rowIndex].filter((arr) => arr.length !== 0);
-      return prev;
-    });
-    if (fieldType === "title") setTitleBlocks((prev) => [movedField, ...prev]);
-    else setUiBlocks((prev) => [movedField, ...prev]);
-    setDragEnd((prev) => !prev); // for check if row or column is not empty to be deleted
-  };
-
-  return (
-    <>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <div className="app">
-          <Sidebar
-            edit={true}
-            title="Záložky"
-            bgColor={"#e2e2e2"}
-            nextBgColor={
-              edit ? "#fff" : "#e2e2e2"
-            }
-          >
-            <Marks edit={edit}/>
-          </Sidebar>
-
-          <Sidebar
-            edit={edit}
-            title="UI bloky"
-            bgColor={"#fff"}
-            nextBgColor={"#e2e2e2"}
-          >
-            <UiBlockPanel edit={edit} uiBlocks={uiBlocks} />
-          </Sidebar>
-
-          <Sidebar
-            edit={edit}
-            title="Nadpisy"
-            bgColor={"#e2e2e2"}
-            nextBgColor={"#e2e2e2"}
-          >
-            <TitleForm setTitleBlocks={setTitleBlocks}/>
-            <TitlePanel
-              edit={edit}
-              titleBlocks={titleBlocks}
-              setTitleBlocks={setTitleBlocks}
-            />
-          </Sidebar>
-
-          <div className="right-panel-wrapper">
-            <EditButtons
-              save={() => buildAreaToSchemaMapper(buildArea)}
-              setEdit={setEdit}
-              edit={edit}
-            />
-            <BuildArea
-              edit={edit}
-              buildArea={buildArea}
-              deleteField={deleteField}
-              setBuildArea={setBuildArea}
-            />
-          </div>
-        </div>
-      </DragDropContext>
-    </>
-  );
+  return <UIGenerator schema={data.schema} UIschema={data.UISchema} />;
 }
 
 export default App;
---
+++ src/UIGenerator/UIGenerator.js
@@ -0,0 +1,107 @@
+import { useState, useEffect } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+import "./UIGenerator.css";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Marks from "../components/Marks/Marks";
+import UiBlockPanel from "../components/UiBlockPanel/UiBlockPanel";
+import TitleForm from "../components/TitlePanel/TitleForm/TitleForm";
+import TitlePanel from "../components/TitlePanel/TitlePanel";
+import BuildArea from "../components/BuildArea/BuildArea";
+import EditButtons from "../components/EditButtons/EditButtons";
+import { onDragEnd } from "../utils/onDragEnd";
+import buildAreaToSchemaMapper from "../utils/buildAreaToSchemaMapper";
+
+export default function UIGenerator({schema}) {
+  const [uiBlocks, setUiBlocks] = useState(schema);
+  const [titleBlocks, setTitleBlocks] = useState([]);
+  const [buildArea, setBuildArea] = useState([]);
+  const [edit, setEdit] = useState(true);
+  const [dragEnd, setDragEnd] = useState(false);
+
+  useEffect(() => {
+    setBuildArea((prev) => {
+      return prev
+        .map((row) => row.filter((col) => col.length !== 0)) // Filter out empty columns
+        .filter((row) => row.length !== 0); // Filter out empty rows
+    });
+  }, [dragEnd]);
+
+  const handleOnDragEnd = (result) => {
+    onDragEnd(
+      result,
+      buildArea,
+      setBuildArea,
+      setDragEnd,
+      uiBlocks,
+      titleBlocks
+    );
+  };
+
+  const deleteField = (rowIndex, columnIndex, fieldIndex, fieldType) => {
+    const movedField = buildArea[rowIndex][columnIndex][fieldIndex];
+    setBuildArea((prev) => {
+      prev[rowIndex][columnIndex] = prev[rowIndex][columnIndex].filter(
+        (_, fieldID) => fieldID !== fieldIndex
+      );
+      prev[rowIndex] = prev[rowIndex].filter((arr) => arr.length !== 0);
+      return prev;
+    });
+    if (fieldType === "title") setTitleBlocks((prev) => [movedField, ...prev]);
+    else setUiBlocks((prev) => [movedField, ...prev]);
+    setDragEnd((prev) => !prev); // for check if row or column is not empty to be deleted
+  };
+
+  return (
+    <>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <div className="UIGenerator">
+          <Sidebar
+            edit={true}
+            title="Záložky"
+            bgColor={"#e2e2e2"}
+            nextBgColor={edit ? "#fff" : "#e2e2e2"}
+          >
+            <Marks edit={edit} />
+          </Sidebar>
+
+          <Sidebar
+            edit={edit}
+            title="UI bloky"
+            bgColor={"#fff"}
+            nextBgColor={"#e2e2e2"}
+          >
+            <UiBlockPanel edit={edit} uiBlocks={uiBlocks} />
+          </Sidebar>
+
+          <Sidebar
+            edit={edit}
+            title="Nadpisy"
+            bgColor={"#e2e2e2"}
+            nextBgColor={"#e2e2e2"}
+          >
+            <TitleForm setTitleBlocks={setTitleBlocks} />
+            <TitlePanel
+              edit={edit}
+              titleBlocks={titleBlocks}
+              setTitleBlocks={setTitleBlocks}
+            />
+          </Sidebar>
+
+          <div className="right-panel-wrapper">
+            <EditButtons
+              save={() => buildAreaToSchemaMapper(buildArea)}
+              setEdit={setEdit}
+              edit={edit}
+            />
+            <BuildArea
+              edit={edit}
+              buildArea={buildArea}
+              deleteField={deleteField}
+              setBuildArea={setBuildArea}
+            />
+          </div>
+        </div>
+      </DragDropContext>
+    </>
+  );
+}
