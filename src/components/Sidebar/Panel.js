e404bde5d77a6a4049246e287d16162c4ae48419
xtejbus <xtejbus@fi.muni,cz>
2024-07-04 10:51:51 +0200

    sidebar components shit fixed

--- src/components/Sidebar/Panel.js
+++ src/components/Sidebar/Panel.js
@@ -3,26 +3,26 @@ import "./Panel.css";
 import PanelOpenClose from "./PanelOpenClose";
 
 const Panel = ({ title, even, children, orientation }) => {
-    const [sidePanel, setSidePanel] = useState(true);
-    const horizontal = orientation === "horizontal" ? "horizontal-" : "";
-    const isHorizontal = orientation === "horizontal" ? true : false;
+  const [sidePanel, setSidePanel] = useState(true);
+  const horizontal = orientation === "horizontal" ? "horizontal-" : "";
+  const isHorizontal = orientation === "horizontal" ? true : false;
 
-    return (
-        <div
-          className={
-            `${horizontal}side-panel ${sidePanel
-            ? `${horizontal}expanded`
-            : `${horizontal}collapsed`} ${even ? "even" : "odd"}`
-        }
-          onClick={!sidePanel ? () => setSidePanel(!sidePanel) : undefined}
-        >
-          {sidePanel ? (
-            <PanelOpenClose title={title} children={children} isHorizontal={isHorizontal} setSidePanel={setSidePanel} isOpen={!sidePanel}/>
-          ) : (
-            <PanelOpenClose title={title} children={children} isHorizontal={isHorizontal} setSidePanel={setSidePanel} isOpen={!sidePanel}/>
-          )}
-        </div>
-      );
-}
+  return (
+    <div
+      className={`${horizontal}side-panel ${
+        sidePanel ? `${horizontal}expanded` : `${horizontal}collapsed`
+      } ${even ? "even" : "odd"}`}
+      onClick={!sidePanel ? () => setSidePanel(!sidePanel) : undefined}
+    >
+      <PanelOpenClose
+        title={title}
+        children={children}
+        isHorizontal={isHorizontal}
+        setSidePanel={setSidePanel}
+        isOpen={!sidePanel}
+      />
+    </div>
+  );
+};
 
 export default Panel;
